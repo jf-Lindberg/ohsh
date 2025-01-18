e5848f154b85f4c799df47e343b1dcc5b743730c
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2025-01-18 12:55:09 +0100

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	var command string
-	fmt.Scanln(&command)
-	fmt.Printf("%s: command not found", command)
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
+	}
 }
